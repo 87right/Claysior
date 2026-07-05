6531437848107bd5c4e73b8476b70b71674a91f1
87right <yuuki.a.181124@gmail.com>
2026-07-05 13:25:10 +0900

    add grid messages

--- src/grid/messages.rs
+++ src/grid/messages.rs
@@ -1,1 +1,18 @@
 //! # Path: src/messages.rs
+
+use bevy::prelude::*;
+
+#[derive(Message)]
+pub struct RightClicked (pub Entity);
+
+#[derive(Message)]
+pub struct LeftClicked (pub Entity);
+
+#[derive(Message)]
+pub struct NetworkUpdated (pub Entity);
+
+#[derive(Message)]
+pub struct Placed (pub Entity);
+
+#[derive(Message)]
+pub struct Broken (pub Entity);
--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -2,3 +2,4 @@
 
 pub mod components;
 pub mod plugins;
+pub mod messages;
