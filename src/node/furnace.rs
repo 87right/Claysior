935f9b65912a8893ebfac33f0de29b5b1aa736d8
87right <211973680+87right@users.noreply.github.com>
2026-07-29 19:08:02 +0900

    feat: Furnace に Port を持たせた

--- src/node/furnace.rs
+++ src/node/furnace.rs
@@ -9,15 +9,20 @@ use crate::{
     },
     consumable::{
         component::*,
+        common::*,
     },
     item::{
         component::*,
     }
 };
 
+const INPUT: SlotID = SlotID(0);
+const OUTPUT: SlotID = SlotID(1);
+
 #[derive(Component, Debug)]
 pub struct ClayFurnace {
-    timer: Timer
+    timer: Timer,
+    running: bool,
 }
 impl BasicNode for ClayFurnace {
     fn get_id() -> String {
@@ -38,9 +43,18 @@ impl BasicNode for ClayFurnace {
         commands.entity(entity).insert((
             ClayFurnace {
                 timer: Timer::from_seconds(10., TimerMode::Once),
+                running: false,
             },
             TextureBuff("textures/tile/clay_furnace_0.png".to_string()),
             Inventory::<Item>::new(2),
+            Channel::<Item>::default().add_port(
+                PortType::Input, 
+                Port::default().set_target_grid(
+                    TargetGrid::Any
+                ).set_target_slot(
+                    TargetSlot::Specific(INPUT)
+                )
+            )
         ));
     }
 }
@@ -61,27 +75,39 @@ fn register_recipe(
 }
 
 fn check_recipe(
+    mut commands: Commands,
     furnace_q: Query<(&mut ClayFurnace, &mut Inventory<Item>, Entity)>,
     recipe: Res<FurnaceRecipe>,
     time: Res<Time>,
 ) {
-    for (mut furnace, mut inventory, _) in furnace_q {
+    for (mut furnace, mut inventory, e) in furnace_q {
         let item;
         if let Some(stick) = inventory.get(SlotID(0)).and_then(|x| x.val)
-        && let Some(out) = recipe.0.get(&RecipeInputBuff(stick)) 
-        && furnace.timer.tick(time.delta()).just_finished() {
-            furnace.timer.reset();
-            item = out.0;
+        && let Some(out) = recipe.0.get(&RecipeInputBuff(stick)) {
+            if !furnace.running {
+                furnace.running = true;
+                commands.entity(e).insert(TextureBuff("textures/tile/clay_furnace_1.png".to_string()));
+            }
+            if furnace.timer.tick(time.delta()).just_finished() {
+                furnace.timer.reset();
+                item = out.0;
+            } else {
+                continue;
+            }
         } else {
+            if furnace.running {
+                furnace.running = false;
+                commands.entity(e).insert(TextureBuff("textures/tile/clay_furnace_0.png".to_string()));
+            }
             continue;
         }
         let mut can_insert = false;
-        if let Some(slot) = inventory.get_mut(SlotID(1)) 
+        if let Some(slot) = inventory.get_mut(OUTPUT) 
         && slot.insert(&mut MaterialSlot::<Item>{val: Some(item), vol: 1}){
             can_insert = true;
         }
         if can_insert 
-        && let Some(slot) = inventory.get_mut(SlotID(0)) {
+        && let Some(slot) = inventory.get_mut(INPUT) {
             slot.vol -= 1;
             if slot.vol == 0 {
                 slot.val = None;
